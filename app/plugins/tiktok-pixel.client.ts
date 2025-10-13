0bc89def636f57ef2c6f4c1cf9093da5ddaceeb5
Mattis BETOURNE <mattis.betourne@gmail.com>
2025-10-13 21:12:13 +0200

    feat: enhance TikTok Pixel integration with improved method handling and dynamic script loading

--- app/plugins/tiktok-pixel.client.ts
+++ app/plugins/tiktok-pixel.client.ts
@@ -4,9 +4,9 @@ export default defineNuxtPlugin(() => {
 
   const loadPixel = () => {
     if (!pixelId || (window as any).ttq?.loaded) return
-    ;(function (w: any, d: Document, t: string) {
-      w.TiktokAnalyticsObject = 'ttq'
-      const ttq = (w.ttq = w.ttq || [])
+    ;(function (w: any, d: Document, libName: string) {
+      w.TiktokAnalyticsObject = libName
+      const ttq = (w[libName] = w[libName] || [])
       ttq.loaded = true
       ttq.methods = [
         'page',
@@ -18,8 +18,13 @@ export default defineNuxtPlugin(() => {
         'off',
         'once',
         'ready',
-        'setUserProperties',
-        'upload',
+        'alias',
+        'group',
+        'enableCookie',
+        'disableCookie',
+        'holdConsent',
+        'revokeConsent',
+        'grantConsent',
       ]
       ttq.setAndDefer = (target: any, method: string) => {
         target[method] = function () {
@@ -27,24 +32,36 @@ export default defineNuxtPlugin(() => {
         }
       }
       for (let i = 0; i < ttq.methods.length; i++) ttq.setAndDefer(ttq, ttq.methods[i])
+      ttq.instance = function (id: string) {
+        const inst = ttq._i[id] || []
+        for (let n = 0; n < ttq.methods.length; n++) ttq.setAndDefer(inst, ttq.methods[n])
+        return inst
+      }
       ttq.load = (id: string, n?: any) => {
+        const src = 'https://analytics.tiktok.com/i18n/pixel/events.js'
         ttq._i = ttq._i || {}
         ttq._i[id] = []
-        ttq._i[id]._u = 'https://analytics.tiktok.com/i18n/pixel/events.js'
+        ttq._i[id]._u = src
         ttq._t = ttq._t || {}
         ttq._t[id] = +new Date()
         ttq._o = ttq._o || {}
         ttq._o[id] = n || {}
-        const s = d.createElement(t)
+        const s = d.createElement('script')
         s.type = 'text/javascript'
         s.async = true
-        s.src = `https://analytics.tiktok.com/i18n/pixel/events.js?sdkid=${id}&lib=ttq`
-        const a = d.getElementsByTagName(t)[0]
-        a.parentNode?.insertBefore(s, a)
+        s.src = `${src}?sdkid=${id}&lib=${libName}`
+        const firstScript = d.getElementsByTagName('script')[0]
+        if (firstScript && firstScript.parentNode) {
+          firstScript.parentNode.insertBefore(s, firstScript)
+        } else if (d.head) {
+          d.head.appendChild(s)
+        } else if (d.body) {
+          d.body.appendChild(s)
+        }
       }
       ttq.load(pixelId)
       ttq.page()
-    })(window, document, 'script')
+    })(window, document, 'ttq')
   }
 
   const loadWhenMarketingConsent = () => {
